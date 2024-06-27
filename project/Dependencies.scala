c27425b83b6b0c19371c9f6304268505a57da6a6
Vince Reuter <vince.reuter@gmail.com>
2024-06-27 19:07:24 +0200

    add zarr module from DNA DSB project

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,6 +15,9 @@ object Dependencies {
     lazy val fs2IO = "co.fs2" %% "fs2-io" % "3.10.2"
     lazy val os = "com.lihaoyi" %% "os-lib" % "0.10.2"
 
+    /** ZARR dependencies */
+    lazy val jzarr = "dev.zarr" % "jzarr" % "0.4.2"
+
     /* Test dependencies */
     lazy val scalacheck = "org.scalacheck" %% "scalacheck" % "1.18.0"
     lazy val scalactic = "org.scalactic" %% "scalactic" % scalatestVersion
