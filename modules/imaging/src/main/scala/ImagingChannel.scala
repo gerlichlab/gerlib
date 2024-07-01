6a74936ca962f8ea1cde1f6c03cb051235fa642d
Vince Reuter <vince.reuter@gmail.com>
2024-07-01 17:42:45 +0200

    use RefinedTypeOps from iron

--- modules/imaging/src/main/scala/ImagingChannel.scala
+++ modules/imaging/src/main/scala/ImagingChannel.scala
@@ -2,17 +2,19 @@ package at.ac.oeaw.imba.gerlich.gerlib.imaging
 
 import cats.*
 import cats.derived.*
+import cats.syntax.all.*
 import at.ac.oeaw.imba.gerlich.gerlib.numeric.*
 
 // Seems to be needed to get the Order[Int :| Nonnegative] instance
 import at.ac.oeaw.imba.gerlich.gerlib.numeric.NonnegativeInt.given
 
 /** Semantic wrapper around value representing 0-based imaging channel */
-final case class ImagingChannel(get: NonnegativeInt) derives Order, Show
+final case class ImagingChannel(get: NonnegativeInt) derives Order
 
 /** Helpers for working with imaging channels */
 object ImagingChannel:
     /** Wrap the given value as an imaging channel, if it's valid as one. */
     def parse: String => Either[String, ImagingChannel] = 
         parseThroughNonnegativeInt("ImagingChannel")(ImagingChannel.apply)
+    given showForImagingChannel: Show[ImagingChannel] = Show.show(_.get.show)
 end ImagingChannel
