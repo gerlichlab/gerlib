c779c0f2a5f2f1616f90f59f4e75a42a7851ee42
Vince Reuter <vince.reuter@gmail.com>
2024-07-04 19:23:04 +0200

    remove unnecessary, overly restrictive need for proof of Numeric instance

--- modules/geometry/src/main/scala/Coordinate.scala
+++ modules/geometry/src/main/scala/Coordinate.scala
@@ -1,30 +1,25 @@
 package at.ac.oeaw.imba.gerlich.gerlib.geometry
 
-// to provide .toDouble syntax on a provably-Numeric
-import scala.math.Numeric.Implicits.infixNumericOps
 import scala.util.NotGiven
-
 import cats.*
 import cats.syntax.all.*
 
 /** Type wrapper around a numeric value representing a spatial coordinate */
 sealed trait Coordinate[A]:
-    /** Access the underling, wrapped value. Requires a [[scala.math.Numeric]] instance to reflect intended use. */
-    def get(using Numeric[A]): A
-    /** With proof that the underlying, wrapped value is numeric, convert it to a [[scala.Double]]  */
-    def toDouble(using Numeric[A]): Double = get.toDouble
+    /** Access the underling, wrapped value. */
+    def get: A
 
 /** Wrap the given value as reprsenting a x-coordinate in space. */
 final case class XCoordinate[A](private val value: A) extends Coordinate[A]:
-    def get(using Numeric[A]): A = value
+    def get: A = value
 
 /** Wrap the given value as reprsenting a y-coordinate in space. */
 final case class YCoordinate[A](private val value: A) extends Coordinate[A]:
-    def get(using Numeric[A]): A = value
+    def get: A = value
 
 /** Wrap the given value as reprsenting a z-coordinate in space. */
 final case class ZCoordinate[A](private val value: A) extends Coordinate[A]:
-    def get(using Numeric[A]): A = value
+    def get: A = value
 
 /** Helpers for working with coordinates in space */
 object Coordinate:
