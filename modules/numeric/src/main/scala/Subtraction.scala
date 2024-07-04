0a23926924ab2232987953c8dc4aa28e154bad69
Vince Reuter <vince.reuter@gmail.com>
2024-07-04 19:22:25 +0200

    add notion of proof of subtractability, useful for subtracting value of wider type from narrower type

---
+++ modules/numeric/src/main/scala/Subtraction.scala
@@ -0,0 +1,14 @@
+package at.ac.oeaw.imba.gerlich.gerlib.numeric
+
+import io.github.iltotore.iron.:|
+
+trait Subtraction[Min, Sub, Result]:
+    infix def minus(minuend: Min)(subtrahend: Sub): Result
+
+object Subtraction:
+    import scala.math.Numeric.Implicits.infixNumericOps
+    given subtractionForNumeric[A : Numeric]: Subtraction[A, A, A] with
+        infix def minus(minuend: A)(subtrahend: A): A = minuend - subtrahend
+    extension [Min](minuend: Min)
+        infix def minus[Sub, Result](subtrahend: Sub)(using ev: Subtraction[Min, Sub, Result]): Result = 
+            ev.minus(minuend)(subtrahend)
