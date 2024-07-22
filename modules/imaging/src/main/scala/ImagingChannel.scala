2ba95e83b92241298990970806724201673cb7af
Vince Reuter <vince.reuter@gmail.com>
2024-07-22 16:20:02 +0200

    remove duplicated typeclass instances

--- modules/imaging/src/main/scala/ImagingChannel.scala
+++ modules/imaging/src/main/scala/ImagingChannel.scala
@@ -3,7 +3,6 @@ package at.ac.oeaw.imba.gerlich.gerlib.imaging
 import cats.*
 import cats.derived.*
 
-import at.ac.oeaw.imba.gerlich.gerlib.SimpleShow
 import at.ac.oeaw.imba.gerlich.gerlib.numeric.*
 import at.ac.oeaw.imba.gerlich.gerlib.numeric.instances.nonnegativeInt.given
 import at.ac.oeaw.imba.gerlich.gerlib.syntax.all.*
@@ -17,6 +16,4 @@ object ImagingChannel:
   /** Wrap the given value as an imaging channel, if it's valid as one. */
   def parse: String => Either[String, ImagingChannel] =
     parseThroughNonnegativeInt("ImagingChannel")(ImagingChannel.apply)
-
-  given SimpleShow[ImagingChannel] = SimpleShow.instance(_.get.show_)
 end ImagingChannel
