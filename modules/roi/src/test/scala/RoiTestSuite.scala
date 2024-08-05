747a8eb2f93a8765f094731999b8a620f775958f
Vince Reuter <vince.reuter@gmail.com>
2024-08-05 16:05:54 +0200

    add tests against bad behavior of SimpleShow instances derived by .fromToString, as happened for PositionName

---
+++ modules/roi/src/test/scala/RoiTestSuite.scala
@@ -0,0 +1,12 @@
+package at.ac.oeaw.imba.gerlich.gerlib.roi
+
+import org.scalacheck.*
+
+import at.ac.oeaw.imba.gerlich.gerlib.numeric.PositiveInt
+
+/** Functionality common to ROI-related test suites. */
+trait RoiTestSuite:
+  given Arbitrary[PositiveInt] = Arbitrary {
+    Gen.posNum[Int].map(PositiveInt.unsafe)
+  }
+end RoiTestSuite
---
+++ modules/roi/src/test/scala/TestRoiDiameter.scala
@@ -0,0 +1,43 @@
+package at.ac.oeaw.imba.gerlich.gerlib.roi
+
+import cats.syntax.all.*
+
+import org.scalacheck.*
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.*
+import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
+
+import at.ac.oeaw.imba.gerlich.gerlib.numeric.PositiveInt
+
+/** Tests of behavior of the ROI diameter data type. */
+class TestRoiDiameter
+    extends AnyFunSuite,
+      ScalaCheckDrivenPropertyChecks,
+      should.Matchers,
+      RoiTestSuite:
+
+  test("RoiDiameter.fromInt is correct.") {
+    forAll { (z: Int) =>
+      val result = RoiDiameter.fromInt(z)
+      val expect =
+        if (z <= 0) {
+          "Should be strictly positive".asLeft
+        } else if (z % 2 =!= 0) {
+          s"Can't use odd value as ROI diameter: $z".asLeft
+        } else {
+          z.asRight
+        }
+      result shouldEqual expect
+    }
+  }
+
+  test("RoiDiameter.fromPositiveInteger is correct.") {
+    forAll { (n: PositiveInt) =>
+      RoiDiameter.fromPositiveInteger(n) match {
+        case None if (n % 2 =!= 0)    => succeed
+        case Some(d) if (n % 2 === 0) => d shouldEqual n
+        case result => fail(s"RoiDiameter.fromPositiveInteger($n) --> $result")
+      }
+    }
+  }
+end TestRoiDiameter
