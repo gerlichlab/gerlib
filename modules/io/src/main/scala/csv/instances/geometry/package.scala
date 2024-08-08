981a9c19c794cac696d7cb74cd6fcad578de87f1
Vince Reuter <vince.reuter@gmail.com>
2024-08-08 18:09:09 +0200

    geometry typeclass instances

---
+++ modules/io/src/main/scala/csv/instances/geometry/package.scala
@@ -0,0 +1,5 @@
+package at.ac.oeaw.imba.gerlich.gerlib.io.csv
+package instances
+
+/** IO-related typeclass instances for geometry-related data types */
+package object geometry extends InstancesForGeometry
