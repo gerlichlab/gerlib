ff21ad62d3f1990a07a4c07440539877c2bb3cf3
Vince Reuter <vince.reuter@gmail.com>
2024-08-09 11:51:36 +0200

    hardening types used for reading and writing CSV

--- modules/io/src/main/scala/csv/Columns.scala
+++ modules/io/src/main/scala/csv/Columns.scala
@@ -1,9 +1,20 @@
 package at.ac.oeaw.imba.gerlich.gerlib.io
 package csv
 
+import cats.data.ValidatedNel
+import cats.syntax.all.*
 import fs2.data.csv.*
 
 import at.ac.oeaw.imba.gerlich.gerlib.cell.NuclearDesignation
+import at.ac.oeaw.imba.gerlich.gerlib.geometry.{
+  XCoordinate,
+  YCoordinate,
+  ZCoordinate
+}
+import at.ac.oeaw.imba.gerlich.gerlib.imaging.ImagingTimepoint
+import at.ac.oeaw.imba.gerlich.gerlib.imaging.FieldOfViewLike
+import at.ac.oeaw.imba.gerlich.gerlib.imaging.ImagingChannel
+import at.ac.oeaw.imba.gerlich.gerlib.roi.measurement.{Area, MeanIntensity}
 
 /** A name corresponding to a CSV file column */
 trait ColumnNameLike[A]:
@@ -11,10 +22,10 @@ trait ColumnNameLike[A]:
   def value: String
 
   /** Try to read a value of type {@code A} from value at key {@code value}. */
-  def pullFrom(row: RowF[Some, String])(using
+  def from(row: RowF[Some, String])(using
       CellDecoder[A]
-  ): DecoderResult[A] =
-    row.as[A](value)
+  ): ValidatedNel[String, A] =
+    row.as[A](value).leftMap(_.getMessage).toValidatedNel
 end ColumnNameLike
 
 /** The name of a column from which an {@code A} is extracted.
@@ -28,8 +39,35 @@ final case class ColumnName[A](value: String) extends ColumnNameLike[A]
 
 /** Collection of names of critical columns from which to parse data */
 object ColumnNames:
-  /** Columns for encoding attribution/designation of nucleus */
+  /** New column name for designation of a record's nucleus */
   val NewNucleusDesignationColumnName =
     ColumnName[NuclearDesignation]("nucleusNumber")
+
+  /** Original column name for designation of a record's nucleus */
   val OldNucleusDesignationColumnName =
     ColumnName[NuclearDesignation]("nuc_label")
+
+  /** Key for field of view */
+  val FieldOfViewColumnName = ColumnName[FieldOfViewLike]("fov")
+
+  /** Key for field of imaging timepont */
+  val TimepointColumnName = ColumnName[ImagingTimepoint]("time")
+
+  /** Key for field of imaging channel */
+  val ChannelColumnName = ColumnName[ImagingChannel]("roi_channel")
+
+  /** Key for z-coordinate of spot centroid */
+  def zCenterColumnName[C] = ColumnName[ZCoordinate[C]]("zc")
+
+  /** Key for x-coordinate of spot centroid */
+  def yCenterColumnName[C] = ColumnName[YCoordinate[C]]("yc")
+
+  /** Key for x-coordinate of spot centroid */
+  def xCenterColumnName[C] = ColumnName[XCoordinate[C]]("xc")
+
+  /** Key for area of 2D detected spot */
+  val AreaColumnName = ColumnName[Area]("area")
+
+  /** Key for the mean pixel intensity value within a detected spot */
+  val IntensityColumnName = ColumnName[MeanIntensity]("intensity_mean")
+end ColumnNames
--- modules/io/src/main/scala/csv/instances/InstancesForRoi.scala
+++ modules/io/src/main/scala/csv/instances/InstancesForRoi.scala
@@ -7,39 +7,11 @@ import fs2.data.csv.*
 
 import at.ac.oeaw.imba.gerlich.gerlib.geometry.*
 import at.ac.oeaw.imba.gerlich.gerlib.imaging.*
+import at.ac.oeaw.imba.gerlich.gerlib.io.csv.ColumnNames.*
 import at.ac.oeaw.imba.gerlich.gerlib.roi.*
 import at.ac.oeaw.imba.gerlich.gerlib.roi.measurement.*
 
 trait InstancesForRoi:
-
-  /** The keys / column names of input files which correspond to the fields of
-    * the detected spot case class
-    */
-  private enum Key(val get: String):
-    /** Key for field of view */
-    case FieldOfView extends Key("fov")
-
-    /** Key for field of imaging timepont */
-    case Timepoint extends Key("time")
-
-    /** Key for field of imaging channel */
-    case Channel extends Key("roi_channel")
-
-    /** Key for z-coordinate of spot centroid */
-    case CenterZ extends Key("zc")
-
-    /** Key for y-coordinate of spot centroid */
-    case CenterY extends Key("yc")
-
-    /** Key for x-cordinate of spot centroid */
-    case CenterX extends Key("xc")
-
-    /** Key for area of 2D detected spot */
-    case Area extends Key("area")
-
-    /** Key for the mean pixel intensity value within a detected spot */
-    case Intensity extends Key("intensity_mean")
-  end Key
 
   given cellDecoderForArea: CellDecoder[Area] =
     liftToCellDecoder(Area.parse)
@@ -67,27 +39,26 @@ trait InstancesForRoi:
     override def apply(
         row: RowF[Some, String]
     ): DecoderResult[DetectedSpot[C]] =
-      val fovNel = row.as[FieldOfViewLike](Key.FieldOfView.get).toValidatedNel
-      val timeNel = row.as[ImagingTimepoint](Key.Timepoint.get).toValidatedNel
-      val channelNel = row.as[ImagingChannel](Key.Channel.get).toValidatedNel
-      val zNel = row.as[ZCoordinate[C]](Key.CenterZ.get).toValidatedNel
-      val yNel = row.as[YCoordinate[C]](Key.CenterY.get).toValidatedNel
-      val xNel = row.as[XCoordinate[C]](Key.CenterX.get).toValidatedNel
-      val areaNel = row.as[Area](Key.Area.get).toValidatedNel
+      val fovNel = FieldOfViewColumnName.from(row)
+      val timeNel = TimepointColumnName.from(row)
+      val channelNel = ChannelColumnName.from(row)
+      val zNel = zCenterColumnName[C].from(row)
+      val yNel = yCenterColumnName[C].from(row)
+      val xNel = xCenterColumnName[C].from(row)
+      val areaNel = AreaColumnName.from(row)
 
       // Disambiguate b/w NonnegativeReal and MeanIntensity, which are the same here in this scope.
       given decMeanIntensity: CellDecoder[MeanIntensity] =
         cellDecoderForMeanIntensity
 
-      val intensityNel =
-        row.as[MeanIntensity](Key.Intensity.get).toValidatedNel
+      val intensityNel = IntensityColumnName.from(row)
       (fovNel, timeNel, channelNel, zNel, yNel, xNel, areaNel, intensityNel)
         .mapN(DetectedSpot.apply[C])
         .toEither
         .leftMap { es =>
-          val msg =
-            s"${es.size} error(s) decoding detected spot from row ($row): ${es.map(_.getMessage).mkString_("; ")}"
-          DecoderError(msg)
+          DecoderError(
+            s"${es.size} error(s) decoding detected spot from row ($row): ${es.mkString_("; ")}"
+          )
         }
 
   /** Encode the given spot field-by-field, using the column/key/field names
@@ -103,17 +74,17 @@ trait InstancesForRoi:
   ): CsvRowEncoder[DetectedSpot[C], String] with
     override def apply(elem: DetectedSpot[C]): RowF[Some, String] =
       val kvs = NonEmptyList.of(
-        Key.FieldOfView.get -> encFov(elem.fieldOfView),
-        Key.Timepoint.get -> encTime(elem.timepoint),
-        Key.Channel.get -> encCh(elem.channel),
-        Key.CenterZ.get -> encZ(elem.centerZ),
-        Key.CenterY.get -> encY(elem.centerY),
-        Key.CenterX.get -> encX(elem.centerX),
-        Key.Area.get -> summon[CellEncoder[Area]](elem.area),
-        Key.Intensity.get -> cellEncoderForMeanIntensity(
+        FieldOfViewColumnName -> encFov(elem.fieldOfView),
+        TimepointColumnName -> encTime(elem.timepoint),
+        ChannelColumnName -> encCh(elem.channel),
+        zCenterColumnName[C] -> encZ(elem.centerZ),
+        yCenterColumnName[C] -> encY(elem.centerY),
+        xCenterColumnName[C] -> encX(elem.centerX),
+        AreaColumnName -> summon[CellEncoder[Area]](elem.area),
+        IntensityColumnName -> cellEncoderForMeanIntensity(
           elem.intensity
         )
       )
       val (headers, textFields) = kvs.unzip
-      RowF(textFields, Some(headers))
+      RowF(textFields, Some(headers.map(_.value)))
 end InstancesForRoi
