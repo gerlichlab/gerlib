3f8c3576e52e5fe4816b059d013b5e05dbdfff01
Vince Reuter <vince.reuter@gmail.com>
2024-08-14 17:09:57 +0200

    add an all-encompassing trait and object for testing-related instances

--- modules/testing/src/main/scala/package.scala
+++ modules/testing/src/main/scala/package.scala
@@ -1,4 +1,13 @@
 package at.ac.oeaw.imba.gerlich.gerlib
 
 /** Tools and types related to testing */
-package object testing
+package object testing:
+  object all extends AllInstances
+
+  trait AllInstances
+      extends CatsScalacheckInstances,
+        CsvInstances,
+        GeometricInstances,
+        ImagingInstances,
+        NumericInstances,
+        RoiInstances
