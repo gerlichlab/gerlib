dbbae383a2bf05ea9b830dd73ccaf74a27570807
Vince Reuter <vince.reuter@gmail.com>
2024-08-15 13:17:28 +0200

    add Arbitrary instances for some core data types and test their availability according to required imports

---
+++ modules/testing/src/main/scala/RoiInstances.scala
@@ -0,0 +1,58 @@
+package at.ac.oeaw.imba.gerlich.gerlib
+package testing
+
+import scala.util.NotGiven
+import cats.Order
+import cats.syntax.all.*
+
+import org.scalacheck.*
+import at.ac.oeaw.imba.gerlich.gerlib.geometry.*
+import at.ac.oeaw.imba.gerlich.gerlib.imaging.ImagingContext
+import at.ac.oeaw.imba.gerlich.gerlib.numeric.NonnegativeReal
+import at.ac.oeaw.imba.gerlich.gerlib.numeric.PositiveReal
+import at.ac.oeaw.imba.gerlich.gerlib.roi.DetectedSpot
+import at.ac.oeaw.imba.gerlich.gerlib.roi.measurement.{Area, MeanIntensity}
+import at.ac.oeaw.imba.gerlich.gerlib.testing.catsScalacheck.given
+
+/** Testing-related typeclass instances for ROI-related data types */
+trait RoiInstances:
+  given arbitraryForBoundingBox[A](using
+      ordA: Order[A],
+      arbZ: Arbitrary[ZCoordinate[A]],
+      arbY: Arbitrary[YCoordinate[A]],
+      arbX: Arbitrary[XCoordinate[A]]
+  ): Arbitrary[BoundingBox[A]] = (
+    arbitraryInterval[A, XCoordinate[A]],
+    arbitraryInterval[A, YCoordinate[A]],
+    arbitraryInterval[A, ZCoordinate[A]]
+  ).mapN(BoundingBox.apply)
+
+  given arbitraryForDetectedSpot[C](using
+      arbCtx: Arbitrary[ImagingContext],
+      arbCenter: Arbitrary[Centroid[C]],
+      arbArea: Arbitrary[Area],
+      arbIntensity: Arbitrary[MeanIntensity]
+  ): Arbitrary[DetectedSpot[C]] =
+    (arbCtx, arbCenter, arbArea, arbIntensity).mapN(DetectedSpot.apply)
+
+  given arbitraryForArea(using
+      arbRaw: Arbitrary[PositiveReal]
+  ): Arbitrary[Area] =
+    arbRaw.map(Area.apply)
+
+  given arbitraryForMeanIntensity(using
+      arbRaw: Arbitrary[NonnegativeReal]
+  ): Arbitrary[MeanIntensity] =
+    arbRaw.map(MeanIntensity.apply)
+
+  /** Generate an arbitrary interval along a particular axis. */
+  private def arbitraryInterval[A: Order, C <: Coordinate[A]: [C] =>> NotGiven[
+    C =:= Coordinate[A]
+  ]](using arbC: Arbitrary[C]): Arbitrary[BoundingBox.Interval[A, C]] =
+    Arbitrary {
+      Arbitrary
+        .arbitrary[(C, C)]
+        .map { case t @ (a, b) => if a > b then b -> a else t }
+        .map(BoundingBox.Interval.apply[A, C])
+    }
+end RoiInstances
---
+++ modules/testing/src/test/scala/TestInstanceAvailability.scala
@@ -0,0 +1,93 @@
+package at.ac.oeaw.imba.gerlich.gerlib.testing
+
+import org.scalacheck.Arbitrary // Save importing this in every snippet.
+import org.scalatest.funsuite.AnyFunSuite
+
+/** Tests for availability of typeclass instances */
+class TestInstanceAvailability extends AnyFunSuite:
+  test("Arbitrary[FieldOfViewLike] is correctly available.") {
+    import at.ac.oeaw.imba.gerlich.gerlib.imaging.FieldOfViewLike // Save importing this in every snippet.
+    assertTypeError { "summon[Arbitrary[FieldOfViewLike]]" }
+    assertTypeError {
+      "import io.github.iltotore.iron.scalacheck.all.given; summon[Arbitrary[FieldOfViewLike]]"
+    }
+    assertTypeError {
+      "import at.ac.oeaw.imba.gerlich.gerlib.testing.all.given; summon[Arbitrary[FieldOfViewLike]]"
+    }
+    assertCompiles {
+      "import io.github.iltotore.iron.scalacheck.all.given; import at.ac.oeaw.imba.gerlich.gerlib.testing.all.given; summon[Arbitrary[FieldOfViewLike]]"
+    }
+  }
+
+  test("Arbitrary[ImagingTimepoint] is correctly available.") {
+    import at.ac.oeaw.imba.gerlich.gerlib.imaging.ImagingTimepoint // Save importing this in every snippet.
+    assertTypeError { "summon[Arbitrary[ImagingTimepoint]]" }
+    assertTypeError {
+      "import io.github.iltotore.iron.scalacheck.all.given; summon[Arbitrary[ImagingTimepoint]]"
+    }
+    assertCompiles {
+      "import at.ac.oeaw.imba.gerlich.gerlib.testing.all.given; summon[Arbitrary[ImagingTimepoint]]"
+    }
+  }
+
+  test("Arbitrary[ImagingChannel] is correctly available.") {
+    import at.ac.oeaw.imba.gerlich.gerlib.imaging.ImagingChannel // Save importing this in every snippet.
+    assertTypeError { "summon[Arbitrary[ImagingChannel]]" }
+    assertTypeError {
+      "import io.github.iltotore.iron.scalacheck.all.given; summon[Arbitrary[ImagingChannel]]"
+    }
+    assertCompiles {
+      "import at.ac.oeaw.imba.gerlich.gerlib.testing.all.given; summon[Arbitrary[ImagingChannel]]"
+    }
+  }
+
+  test("Arbitrary[ImagingContext] is correctly available.") {
+    import at.ac.oeaw.imba.gerlich.gerlib.imaging.ImagingContext // Save importing this in every snippet.
+    assertTypeError { "summon[Arbitrary[ImagingContext]]" }
+    assertTypeError {
+      "import io.github.iltotore.iron.scalacheck.all.given; summon[Arbitrary[ImagingContext]]"
+    }
+    assertTypeError {
+      "import at.ac.oeaw.imba.gerlich.gerlib.testing.all.given; summon[Arbitrary[ImagingContext]]"
+    }
+    assertCompiles {
+      "import io.github.iltotore.iron.scalacheck.all.given; import at.ac.oeaw.imba.gerlich.gerlib.testing.all.given; summon[Arbitrary[ImagingContext]]"
+    }
+  }
+
+  test("Arbitrary[Area] is correctly available.") {
+    import at.ac.oeaw.imba.gerlich.gerlib.roi.measurement.Area // Save importing this in every snippet.
+    assertTypeError { "summon[Arbitrary[Area]]" }
+    assertTypeError {
+      "import io.github.iltotore.iron.scalacheck.all.given; summon[Arbitrary[Area]]"
+    }
+    assertCompiles {
+      "import at.ac.oeaw.imba.gerlich.gerlib.testing.all.given; summon[Arbitrary[Area]]"
+    }
+  }
+
+  test("Arbitrary[MeanIntensity] is correctly available.") {
+    import at.ac.oeaw.imba.gerlich.gerlib.roi.measurement.MeanIntensity // Save importing this in every snippet.
+    assertTypeError { "summon[Arbitrary[MeanIntensity]]" }
+    assertTypeError {
+      "import io.github.iltotore.iron.scalacheck.all.given; summon[Arbitrary[MeanIntensity]]"
+    }
+    assertCompiles {
+      "import at.ac.oeaw.imba.gerlich.gerlib.testing.all.given; summon[Arbitrary[MeanIntensity]]"
+    }
+  }
+
+  test("Arbitrary[DetectedSpot[Double]] is correctly available.") {
+    import at.ac.oeaw.imba.gerlich.gerlib.roi.DetectedSpot // Save importing this in every snippet.
+    assertTypeError { "summon[Arbitrary[DetectedSpot[Double]]]" }
+    assertTypeError {
+      "import io.github.iltotore.iron.scalacheck.all.given; summon[Arbitrary[DetectedSpot[Double]]]"
+    }
+    assertTypeError {
+      "import at.ac.oeaw.imba.gerlich.gerlib.testing.all.given; summon[Arbitrary[DetectedSpot[Double]]]"
+    }
+    assertCompiles {
+      "import io.github.iltotore.iron.scalacheck.all.given; import at.ac.oeaw.imba.gerlich.gerlib.testing.all.given; summon[Arbitrary[DetectedSpot[Double]]]"
+    }
+  }
+end TestInstanceAvailability
