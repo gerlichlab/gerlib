b8767cee0cff21f489442deb53210f2dbfa888e5
Vince Reuter <vince.reuter@gmail.com>
2024-08-20 15:17:41 +0200

    more CSV codec-related types

--- modules/io/src/main/scala/csv/instances/InstancesForCell.scala
+++ modules/io/src/main/scala/csv/instances/InstancesForCell.scala
@@ -6,9 +6,26 @@ import fs2.data.csv.*
 import at.ac.oeaw.imba.gerlich.gerlib.cell.*
 import at.ac.oeaw.imba.gerlich.gerlib.syntax.all.*
 
+/** Typeclass instances and helper functions for cell-related data types */
 trait InstancesForCell:
   given cellDecoderForNuclearDesignation: CellDecoder[NuclearDesignation] =
     liftToCellDecoder(NuclearDesignation.parse)
 
   given cellEncoderForNuclearDesignation: CellEncoder[NuclearDesignation] with
     override def apply(cell: NuclearDesignation): String = cell.show_
+
+  def getCsvRowDecoderForNuclearDesignation(
+      key: ColumnNameLike[NuclearDesignation] =
+        ColumnNames.NucleusDesignationColumnName
+  )(using
+      CellDecoder[NuclearDesignation]
+  ): CsvRowDecoder[NuclearDesignation, String] =
+    getCsvRowDecoderForSingleton(key)
+
+  def getCsvRowEncoderForNuclearDesignation(
+      key: ColumnNameLike[NuclearDesignation] =
+        ColumnNames.NucleusDesignationColumnName
+  )(using
+      CellEncoder[NuclearDesignation]
+  ): CsvRowEncoder[NuclearDesignation, String] =
+    getCsvRowEncoderForSingleton(key)
