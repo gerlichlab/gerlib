b4ce53b0a92f586d94e12de2d908518006b8eb64
Vince Reuter <vince.reuter@gmail.com>
2024-08-22 15:09:17 +0200

    make 3D point a member of Monoid when the underlying value type is a member of Monoid

---
+++ modules/geometry/src/main/scala/instances/point/package.scala
@@ -0,0 +1,7 @@
+package at.ac.oeaw.imba.gerlich.gerlib.geometry
+package instances
+
+/** Object to make more convenient imports of typeclass instances related to
+  * geometrical points
+  */
+package object point extends PointInstances
