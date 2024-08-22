8540c88debb4f283852eeb28756607f529dbaf8a
Vince Reuter <vince.reuter@gmail.com>
2024-08-22 15:00:49 +0200

    make the coordinate data types members of Monoid when the underling value type is a member of Monoid

---
+++ modules/geometry/src/main/scala/instances/CoordinateInstances.scala
@@ -0,0 +1,28 @@
+package at.ac.oeaw.imba.gerlich.gerlib.geometry
+package instances
+
+import scala.util.NotGiven
+
+import cats.*
+import cats.syntax.all.*
+
+/** Typeclass instances for coordinate data types */
+trait CoordinateInstances:
+  def instance[A: Monoid, C <: Coordinate[A]: [C] =>> NotGiven[
+    C =:= Coordinate[A]
+  ]](lift: A => C): Monoid[C] = new:
+    override def empty: C = lift(summon[Monoid[A]].empty)
+    override def combine(a: C, b: C): C = lift(a.value |+| b.value)
+
+  given monoidForX[A: Monoid]: Monoid[XCoordinate[A]] = instance(
+    XCoordinate.apply
+  )
+
+  given monoidForY[A: Monoid]: Monoid[YCoordinate[A]] = instance(
+    YCoordinate.apply
+  )
+
+  given monoidForZ[A: Monoid]: Monoid[ZCoordinate[A]] = instance(
+    ZCoordinate.apply
+  )
+end CoordinateInstances
---
+++ modules/geometry/src/main/scala/instances/package.scala
@@ -0,0 +1,12 @@
+package at.ac.oeaw.imba.gerlich.gerlib.geometry
+
+/** Typeclass instances for geometry-related data types */
+package object instances:
+  /** Aggregation of all the geometry-related data types' typeclass instances,
+    * for import convenience with .all.given
+    */
+  object all extends AllGeometryInstances
+
+  /** Aggregation of all the geometry-related data types' typeclass instances */
+  trait AllGeometryInstances extends CoordinateInstances
+end instances
