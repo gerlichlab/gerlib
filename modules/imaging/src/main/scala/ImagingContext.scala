c341a3aad67a8605741a6516a568a0aff9fd5ac6
Vince Reuter <vince.reuter@gmail.com>
2024-08-26 15:03:07 +0200

    use derivation of cats.Order for ImagingContext

--- modules/imaging/src/main/scala/ImagingContext.scala
+++ modules/imaging/src/main/scala/ImagingContext.scala
@@ -1,8 +1,11 @@
 package at.ac.oeaw.imba.gerlich.gerlib.imaging
 
+import cats.Order
+import cats.derived.*
+
 /** Context in which an image was captured */
 final case class ImagingContext(
     fieldOfView: FieldOfViewLike,
     timepoint: ImagingTimepoint,
     channel: ImagingChannel
-)
+) derives Order
