c3d342dab9a22e146a386babf42e46749f3e691a
Vince Reuter <vince.reuter@gmail.com>
2024-09-02 18:14:14 +0200

    add the .excludes extension to NonEmptySet as for ordinary Set

--- modules/pan/src/main/scala/collections.scala
+++ modules/pan/src/main/scala/collections.scala
@@ -1,10 +1,15 @@
 package at.ac.oeaw.imba.gerlich.gerlib
 
+import cats.data.NonEmptySet
 import cats.syntax.all.*
 
 /** Tools for working with collections */
 object collections:
   extension [A](bag: Set[A])
+    /** Negation of inclusion/membership test result */
+    def excludes(a: A): Boolean = !bag.contains(a)
+
+  extension [A](bag: NonEmptySet[A])
     /** Negation of inclusion/membership test result */
     def excludes(a: A): Boolean = !bag.contains(a)
 
