e978b014f8220de748701d9ecc3f9ecd77d7a6b0
Vince Reuter <vince.reuter@gmail.com>
2024-09-24 15:13:03 +0200

    expose the wrapped coordinate values, to make downstream ad-hoc new use cases not so cumbersome

--- modules/geometry/src/main/scala/Coordinate.scala
+++ modules/geometry/src/main/scala/Coordinate.scala
@@ -3,16 +3,13 @@ package at.ac.oeaw.imba.gerlich.gerlib.geometry
 /** Type wrapper around a numeric value representing a spatial coordinate */
 sealed trait Coordinate[A]:
   /** Access the underling, wrapped value. */
-  private[gerlib] def value: A
+  def value: A
 
 /** Wrap the given value as reprsenting a x-coordinate in space. */
-final case class XCoordinate[A](private[gerlib] val value: A)
-    extends Coordinate[A]
+final case class XCoordinate[A](value: A) extends Coordinate[A]
 
 /** Wrap the given value as reprsenting a y-coordinate in space. */
-final case class YCoordinate[A](private[gerlib] val value: A)
-    extends Coordinate[A]
+final case class YCoordinate[A](value: A) extends Coordinate[A]
 
 /** Wrap the given value as reprsenting a z-coordinate in space. */
-final case class ZCoordinate[A](private[gerlib] val value: A)
-    extends Coordinate[A]
+final case class ZCoordinate[A](value: A) extends Coordinate[A]
