4bfda29ae59c0924ad80a1e85e241e0bbac8352d
Vince Reuter <vince.reuter@gmail.com>
2024-10-24 15:22:25 +0200

    tests for List as underlying container of AtLeast2

--- modules/pan/src/test/scala/TestAtLeast2.scala
+++ modules/pan/src/test/scala/TestAtLeast2.scala
@@ -12,18 +12,28 @@ import at.ac.oeaw.imba.gerlich.gerlib.collections.AtLeast2
   */
 class TestAtLeast2 extends AnyFunSuite, ScalaCheckPropertyChecks, should.Matchers:
 
-  test("AtLeast2[Set, X] is correct with apply-syntax.") {
-    forAll: (xs: NonEmptySet[Int], x: Int) =>
-      val atLeast2 = AtLeast2(xs, x)
-      atLeast2 `contains` x shouldBe true
-      atLeast2.size shouldBe (xs.length + (if xs `contains` x then 0 else 1))
-  }
+  test("For lists, AtLeast2.apply has cons-like argument order"):
+    assertTypeError:
+      "AtLeast2(NonEmptyList.one(1), 0)"
+    assertCompiles:
+      "AtLeast2(0, NonEmptyList.one(1))"
 
-  test("AtLeast2.apply only compiles for set-then-element argument order.") {
+  test("For sets, AtLeast2.apply only compiles for set-then-element argument order."):
     assertTypeError:
       "AtLeast2(1, NonEmptySet.one(0))"
     assertCompiles:
       "AtLeast2(NonEmptySet.one(0), 1)"
-  }
+
+  test("For lists, AtLeat2 is correct with apply-syntax"):
+    forAll: (xs: NonEmptyList[Int], x: Int) =>
+      val atLeast2 = AtLeast2(x, xs)
+      atLeast2 `contains` x shouldBe true
+      atLeast2.length shouldEqual 1 + xs.length
+
+  test("For sets, AtLeast2 is correct with apply-syntax."):
+    forAll: (xs: NonEmptySet[Int], x: Int) =>
+      val atLeast2 = AtLeast2(xs, x)
+      atLeast2 `contains` x shouldBe true
+      atLeast2.size shouldEqual (xs.length + (if xs `contains` x then 0 else 1))
 
 end TestAtLeast2
