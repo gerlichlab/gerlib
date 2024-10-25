372fc77ccb3181803090e04f73dd00e6f0c8b345
Vince Reuter <vince.reuter@gmail.com>
2024-10-25 15:58:37 +0200

    adding the AtLeast2 data type / abstraction

--- modules/pan/src/test/scala/TestAtLeast2.scala
+++ modules/pan/src/test/scala/TestAtLeast2.scala
@@ -2,6 +2,8 @@ package at.ac.oeaw.imba.gerlich.gerlib
 
 import cats.data.*
 import cats.laws.discipline.arbitrary.given // for Arbitrary[NonEmptySet[*]], Arbitrary[NonEmptyList[*]], etc.
+import io.github.iltotore.iron.Constraint
+import io.github.iltotore.iron.constraint.collection.MinLength
 import org.scalacheck.*
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should
@@ -11,6 +13,26 @@ import at.ac.oeaw.imba.gerlich.gerlib.collections.AtLeast2
 /** Tests for the [[collections.AtLeast2]] refinement type
   */
 class TestAtLeast2 extends AnyFunSuite, ScalaCheckPropertyChecks, should.Matchers:
+
+  inline given arbitraryForAtLeast2[C[*], E](using
+      Arbitrary[E],
+      org.scalacheck.util.Buildable[E, C[E]],
+      C[E] => Iterable[E],
+      Constraint[C[E], MinLength[2]]
+  ): Arbitrary[AtLeast2[C, E]] = Arbitrary {
+    for
+      n <- Gen.choose(2, 5)
+      unrefined <- Gen.containerOfN[C, E](n, Arbitrary.arbitrary[E])
+    yield AtLeast2
+      .either(unrefined)
+      .fold(
+        msg =>
+          throw new Exception(
+            s"Error generating collection of at least 2 elements! Collection: ${unrefined}. Message: $msg"
+          ),
+        identity
+      )
+  }
 
   test("For lists, AtLeast2.apply has cons-like argument order"):
     assertTypeError:
@@ -25,15 +47,30 @@ class TestAtLeast2 extends AnyFunSuite, ScalaCheckPropertyChecks, should.Matcher
       "AtLeast2(NonEmptySet.one(0), 1)"
 
   test("For lists, AtLeat2 is correct with apply-syntax"):
+    import at.ac.oeaw.imba.gerlich.gerlib.collections.AtLeast2.syntax.*
     forAll: (xs: NonEmptyList[Int], x: Int) =>
       val atLeast2 = AtLeast2(x, xs)
       atLeast2 `contains` x shouldBe true
-      atLeast2.length shouldEqual 1 + xs.length
+      atLeast2.size shouldEqual 1 + xs.size
 
   test("For sets, AtLeast2 is correct with apply-syntax."):
+    import at.ac.oeaw.imba.gerlich.gerlib.collections.AtLeast2.syntax.*
     forAll: (xs: NonEmptySet[Int], x: Int) =>
       val atLeast2 = AtLeast2(xs, x)
       atLeast2 `contains` x shouldBe true
       atLeast2.size shouldEqual (xs.length + (if xs `contains` x then 0 else 1))
+
+  test(
+    ".map on a AtLeast2[C, *] value returns a refined value IF AND ONLY IF a functor is available for the underlying container type and the AtLeast2 syntax is imported"
+  ):
+    assertCompiles("summon[cats.Functor[List]]") // Functor is available for List.
+    assertTypeError("summon[cats.Functor[Set]]") // No functor for Set
+    assertCompiles(
+      "AtLeast2.unsafe(List(0, 1, 2))"
+    ) // necessary precondition to render subsequent checks meaningful
+    assertTypeError("AtLeast2.unsafe(List(0, 1, 2)).map(_ + 1)") // Haven't imported the .map syntax
+    assertCompiles(
+      "import AtLeast2.syntax.map; AtLeast2.unsafe(List(0, 1, 2)).map(_ + 1): AtLeast2[List, Int]"
+    ) // With the proper syntax import, the .map operation works.
 
 end TestAtLeast2
