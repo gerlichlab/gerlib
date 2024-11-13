28fb343dabe7f176116e7ade21c6e135bb365552
Vince Reuter <vince.reuter@gmail.com>
2024-11-13 13:14:16 +0100

    add a Scaladoc

--- modules/json/src/main/scala/instances/JsonInstancesForGeometry.scala
+++ modules/json/src/main/scala/instances/JsonInstancesForGeometry.scala
@@ -4,6 +4,7 @@ package instances
 import scala.util.NotGiven
 import at.ac.oeaw.imba.gerlich.gerlib.geometry.Coordinate
 
+/** JSON-related typeclass instances for geometry-related data types */
 trait JsonInstancesForGeometry:
   def getPlainJsonValueWriter[A, C <: Coordinate[
     A
@@ -11,4 +12,6 @@ trait JsonInstancesForGeometry:
       writeRaw: JsonValueWriter[A, O]
   ): JsonValueWriter[C, O] = new:
     override def apply(c: C): O = writeRaw(c.value)
+
+  
 end JsonInstancesForGeometry
