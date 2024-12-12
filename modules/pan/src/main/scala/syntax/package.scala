136b50394a19ff1c86fc8896dd4e2e9e69688853
Vince Reuter <vince.reuter@gmail.com>
2024-12-12 16:56:54 +0100

    add .flipBy and .swapBy on Tuple2

--- modules/pan/src/main/scala/syntax/package.scala
+++ modules/pan/src/main/scala/syntax/package.scala
@@ -1,6 +1,8 @@
 package at.ac.oeaw.imba.gerlich.gerlib
 
+/** Aggregation of syntax enrichments, for convenenience as imports */
 package object syntax:
+  /** Aggregation of syntax enrichments, for convenenience as imports */
   object all extends AllSyntax
 
-  trait AllSyntax extends SyntaxForOption, SyntaxForSimpleShow
+  trait AllSyntax extends SyntaxForOption, SyntaxForSimpleShow, SyntaxForTuple2
---
+++ modules/pan/src/main/scala/syntax/tuple2.scala
@@ -0,0 +1,5 @@
+package at.ac.oeaw.imba.gerlich.gerlib
+package syntax
+
+/** Facilitate the import of the syntax enrichment on Tuple2. */
+package object tuple2 extends SyntaxForTuple2
