2f2b91911a524e462a148fdd3339a7b16fc7b6c7
Vince Reuter <vince.reuter@gmail.com>
2025-04-24 17:20:11 +0200

    add a codec for Distance

--- modules/json/src/main/scala/instances/JsonInstancesForGeometry.scala
+++ modules/json/src/main/scala/instances/JsonInstancesForGeometry.scala
@@ -2,7 +2,11 @@ package at.ac.oeaw.imba.gerlich.gerlib.json
 package instances
 
 import scala.util.NotGiven
-import at.ac.oeaw.imba.gerlich.gerlib.geometry.Coordinate
+import cats.syntax.all.*
+import squants.space.Length
+import upickle.default.*
+import at.ac.oeaw.imba.gerlich.gerlib.geometry.{Coordinate, Distance}
+import at.ac.oeaw.imba.gerlich.gerlib.refinement.IllegalRefinement
 
 /** JSON-related typeclass instances for geometry-related data types */
 trait JsonInstancesForGeometry:
@@ -13,4 +17,11 @@ trait JsonInstancesForGeometry:
   ): JsonValueWriter[C, O] = new:
     override def apply(c: C): O = writeRaw(c.value)
 
+  given (ReadWriter[String]) => ReadWriter[Distance] = readwriter[String].bimap(
+    _.toString,
+    s =>
+      Length(s).toEither
+        .flatMap(l => Distance.either(l).leftMap(msg => IllegalRefinement(l, msg)))
+        .fold(throw _, identity)
+  )
 end JsonInstancesForGeometry
