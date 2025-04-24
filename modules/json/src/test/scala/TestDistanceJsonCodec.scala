3811df02418f5101f02f640b759d606306bec355
Vince Reuter <vince.reuter@gmail.com>
2025-04-24 17:20:11 +0200

    add a codec for Distance

---
+++ modules/json/src/test/scala/TestDistanceJsonCodec.scala
@@ -0,0 +1,59 @@
+package at.ac.oeaw.imba.gerlich.gerlib.json
+package instances
+
+import squants.QuantityParseException
+import squants.space.*
+import upickle.default.*
+import org.scalacheck.*
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+
+import at.ac.oeaw.imba.gerlich.gerlib.geometry.Distance
+import at.ac.oeaw.imba.gerlich.gerlib.refinement.IllegalRefinement
+
+/** Tests for positive integer refinement type */
+class TestDistanceJsonCodec extends AnyFunSuite, should.Matchers, ScalaCheckPropertyChecks:
+  test("Illegal distance fails with expected IllegalRefinement."):
+    import geometry.given // for the ReadWriter instance
+
+    forAll(
+      Table(
+        ("data", "expectedException"),
+        // Distance must carry units.
+        ("70", QuantityParseException("Unable to parse Length", "70")),
+        // Distance cannot be negative.
+        (
+          "-70 nm",
+          IllegalRefinement(
+            unsafeBuildLength(-70, Nanometers),
+            "Allegedly nonnegative length must actually be nonnegative."
+          )
+        )
+      )
+    ) { (data, expectedException) =>
+      val observedException = intercept[expectedException.type] { read[Distance](ujson.Str(data)) }
+      observedException shouldEqual expectedException
+    }
+
+  test("Legal distance roundtrips through JSON"):
+    import geometry.given // for the ReadWriter instance
+
+    given Arbitrary[Distance] = Arbitrary(
+      for
+        u <- Gen.oneOf(Nanometers, Microns, Millimeters)
+        v <- Gen.choose(0.0, Double.MaxValue)
+        l = unsafeBuildLength(v, u)
+      yield Distance
+        .either(l)
+        .fold(_ => throw IllegalRefinement(l, "Cannot refine length as distance"), identity)
+    )
+
+    forAll { (original: Distance) =>
+      val reparsed: Distance = read(write(original))
+      reparsed shouldEqual original
+    }
+
+  private def unsafeBuildLength(value: Int | Double, units: LengthUnit): Length =
+    Length(s"$value ${units.symbol}").fold(throw _, identity)
+end TestDistanceJsonCodec
